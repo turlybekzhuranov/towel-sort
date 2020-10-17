ced705aa1162d7cfd49ccd41b4b080744545ab6f
Turlybek Zhuranov <turlybekk@gmail.com>
2020-10-17 08:33:29 +0600

    feat: update function towelSort

--- src/index.js
+++ src/index.js
@@ -2,5 +2,15 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+  
+      if ( !Array.isArray(matrix) || (matrix.length == 0) ) return ([]);
+    let sortedMatrix = []; //itr = 1;
+
+        matrix.forEach((subMatrix, index) => { 
+          if (index % 2 !== 0) { sortedMatrix = sortedMatrix.concat( subMatrix.reverse() ); }
+          else { sortedMatrix = sortedMatrix.concat(subMatrix); }
+          //itr += 1;
+        });
+
+      return sortedMatrix;
 }
